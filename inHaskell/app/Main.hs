6656e80a39743f68fe6f29fa86eb69eea60cf3b7
Shubham <kumar.shubham1312@gmail.com>
2019-08-03 03:22:27 +0530

    Chapter 1 : Exercise three

--- inHaskell/app/Main.hs
+++ inHaskell/app/Main.hs
@@ -5,12 +5,6 @@ import System.Environment
 
 main :: IO ()
 main = do
-    args <- getArgs
-    let
-        simpleArithmetic :: Int
-        simpleArithmetic = (+) x y where
-            x            = readVal $ args !! 0
-            y            = readVal $ args !! 1
-            readVal :: String -> Int
-            readVal = read
-    putStrLn $ show simpleArithmetic
+    putStr ("Enter your shite : ")
+    myShite <- getLine
+    putStrLn ("This is your shite : " <> myShite)
